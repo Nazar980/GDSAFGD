19ac52c9a7d676f8852ff0547624516246568ab3
Nazar980 <golovysiaut5@gmail.com>
2026-02-18 20:41:52 +0300

    Update MixinKeyBindingListKeyEntry.java

--- common/java/com/zergatul/cheatutils/mixins/common/MixinKeyBindingListKeyEntry.java
+++ common/java/com/zergatul/cheatutils/mixins/common/MixinKeyBindingListKeyEntry.java
@@ -1,11 +1,7 @@
 package com.zergatul.cheatutils.mixins.common;
 
-import com.zergatul.cheatutils.configs.ConfigStore;
 import net.minecraft.client.gui.screens.options.controls.KeyBindsList;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.contents.PlainTextContents;
-import net.minecraft.network.chat.contents.TranslatableContents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -17,18 +13,7 @@ public abstract class MixinKeyBindingListKeyEntry {
             method = "renderContent",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;III)V"))
     private Component onRenderKeyName(Component component) {
-        if (component instanceof MutableComponent mutable) {
-            if (mutable.getContents() instanceof TranslatableContents translatable) {
-                final String prefix = "key.zergatul.cheatutils.reserved";
-                if (translatable.getKey().startsWith(prefix)) {
-                    int index = Integer.parseInt(translatable.getKey().substring(prefix.length()));
-                    String name = ConfigStore.instance.getConfig().keyBindingsConfig.bindings[index];
-                    if (name != null) {
-                        return MutableComponent.create(new PlainTextContents.LiteralContents(index + ": " + name));
-                    }
-                }
-            }
-        }
+        // Просто возвращаем оригинальный компонент без изменений
         return component;
     }
 }
